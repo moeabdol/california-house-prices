db32db9a2ef1b07b14bb54e1f6949d4e638fca5f
Mohammed Saed <mohd.a.saed@gmail.com>
2024-09-23 16:49:05 +0300

    Add dependencies and work on code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Data path
 HOUSING_PATH = os.path.join("data", "housing.csv")
@@ -11,3 +12,13 @@ housing_df.head()
 
 # Show quick descripton of the data
 housing_df.info()
+
+# Show unique values of the categorical field
+housing_df["ocean_proximity"].value_counts()
+
+# Show summary of all numercial values
+housing_df.describe()
+
+# Draw histograms of all numerical attributes
+housing_df.hist(bins=50, figsize=(20, 15))
+plt.show()
