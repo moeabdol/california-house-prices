54da4f5b3d1b9d91880b1a6d71d0100a6f8614e1
Mohammed Saed <mohd.a.saed@gmail.com>
2024-09-25 22:28:44 +0300

    More code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
 
 # Data path
 HOUSING_PATH = os.path.join("data", "housing.csv")
@@ -22,3 +24,22 @@ housing_df.describe()
 # Draw histograms of all numerical attributes
 housing_df.hist(bins=50, figsize=(20, 15))
 plt.show()
+
+# Plot only median income histograms
+housing_df["median_income"].hist()
+plt.show()
+
+# Create income category attribute
+housing_df["income_cat"] = pd.cut(
+            housing_df["median_income"],
+            bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+            labels=[1, 2, 3, 4, 5]
+)
+housing_df["income_cat"].hist()
+plt.show()
+
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing_df, housing_df["income_cat"]):
+    strat_train_set = housing_df.loc[train_index]
+    strat_test_set = housing_df.loc[test_index]
